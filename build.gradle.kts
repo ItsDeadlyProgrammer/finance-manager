46085006c6a38e8cb206787845ef56229fec02b3
ItsDeadlyProgrammer <hvsr29march2004@gmail.com>
2026-05-25 13:22:27 +0530

    switch to railway buildpack deployment

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,9 @@ group = "com.finance"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
 }
 
 repositories {
